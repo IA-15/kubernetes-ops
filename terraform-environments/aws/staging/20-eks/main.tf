99e0e9ad46929dfe94c5a27cad3fe4d5b7794edd
killer <michael.molchanov.2004@gmail.com>
2024-05-29 23:15:27 +0200

    no cert and less capacity

--- terraform-environments/aws/staging/20-eks/main.tf
+++ terraform-environments/aws/staging/20-eks/main.tf
@@ -90,8 +90,8 @@ module "eks" {
     ng1 = {
       version          = "1.29"
       disk_size        = 20
-      desired_capacity = 5
-      max_capacity     = 10
+      desired_capacity = 2
+      max_capacity     = 4
       min_capacity     = 1
       instance_types   = ["t3.micro"]
       additional_tags  = local.tags
